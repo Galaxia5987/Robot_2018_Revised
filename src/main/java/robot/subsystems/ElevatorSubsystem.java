8491fc60fb7eaea05836d6882c323204e0c7b23f
Adam <adam.vichik@gmail.com>
2019-09-13 01:01:35 -0700

    Create convertTicksToHeight()

--- src/main/java/robot/subsystems/ElevatorSubsystem.java
+++ src/main/java/robot/subsystems/ElevatorSubsystem.java
@@ -12,26 +12,37 @@ public class ElevatorSubsystem extends Subsystem {
 
     private TalonSRX liftMaster = new TalonSRX(RobotMap.ELEVATOR_MOTOR);
 
-    public ElevatorSubsystem(){
+    public ElevatorSubsystem() {
 
         liftMaster.configMotionSCurveStrength(4);
-        liftMaster.configSelectedFeedbackSensor(FeedbackDevice.CTRE_MagEncoder_Relative, 0,0);
+        liftMaster.configSelectedFeedbackSensor(FeedbackDevice.CTRE_MagEncoder_Relative, 0, 0);
         liftMaster.setSensorPhase(ElevatorConstants.ENCODER_REVERSED);
         liftMaster.config_kP(ElevatorConstants.TALON_PID_SLOT, 0.2, ElevatorConstants.TALON_TIMEOUT_MS);
         liftMaster.config_kI(ElevatorConstants.TALON_PID_SLOT, 0, ElevatorConstants.TALON_TIMEOUT_MS);
         liftMaster.config_kD(ElevatorConstants.TALON_PID_SLOT, 0, ElevatorConstants.TALON_TIMEOUT_MS);
-        liftMaster.config_kF(ElevatorConstants.TALON_PID_SLOT, 0, ElevatorConstants.TALON_TIMEOUT_MS);
+        liftMaster.configSelectedFeedbackCoefficient(ElevatorConstants.TICKS_PER_METER);
+
     }
+
     /*
     public double getSpeed(){
         return liftMaster.getSelectedSensorVelocity();
     }
 
      */
-    public void setPosition(double position){
+    public void setPosition(double position) {
         liftMaster.set(ControlMode.MotionMagic, position, DemandType.ArbitraryFeedForward, ElevatorConstants.FEED_FORWARD);
-        
+
     }
+
+    public void setHeight() {
+    }
+
+
+    public double convertTicksToHeight() {
+        return liftMaster.getSelectedSensorPosition();
+    }
+
     /*
     public void setSpeed(double position){
         liftMaster.set(ControlMode.MotionMagic, position);
