58c6f07327f3e2b8f5075e9fbb6a6c13ec3574dd
Adam <adam.vichik@gmail.com>
2019-09-13 03:34:12 -0700

    Implement ElevatorSubsystem methods

--- src/main/java/robot/subsystems/Commands/MoveElevator.java
+++ src/main/java/robot/subsystems/Commands/MoveElevator.java
@@ -1,6 +1,10 @@
 package robot.subsystems.Commands;
 
 import edu.wpi.first.wpilibj.command.Command;
+import robot.Robot;
+import robot.RobotMap;
+import robot.subsystems.ElevatorConstants;
+import robot.subsystems.ElevatorSubsystem;
 
 
 public class MoveElevator extends Command {
@@ -13,25 +17,26 @@ public class MoveElevator extends Command {
 
     @Override
     protected void initialize() {
-
+        Robot.elevatorsubsystem.setHeight(targetHeight);
     }
 
     @Override
     protected void execute() {
-
+        Robot.elevatorsubsystem.setHeight(targetHeight);
     }
 
     @Override
     protected boolean isFinished() {
-        return false;
+        return Robot.elevatorsubsystem.getHeight() >= targetHeight - 0.05;
     }
 
     @Override
     protected void interrupted() {
-
+        end();
     }
 
     @Override
     protected void end() {
+        
     }
 }
