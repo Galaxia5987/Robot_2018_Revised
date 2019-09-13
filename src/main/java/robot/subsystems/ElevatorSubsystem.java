4e7c0a65e45774b124d69562ffebed1e4141bf65
Adam <adam.vichik@gmail.com>
2019-09-13 00:35:49 -0700

    Configure MotionMagic and PID values

--- src/main/java/robot/subsystems/ElevatorSubsystem.java
+++ src/main/java/robot/subsystems/ElevatorSubsystem.java
@@ -1,13 +1,43 @@
 package robot.subsystems;
 
+import com.ctre.phoenix.motorcontrol.ControlMode;
+import com.ctre.phoenix.motorcontrol.DemandType;
+import com.ctre.phoenix.motorcontrol.FeedbackDevice;
 import com.ctre.phoenix.motorcontrol.can.TalonSRX;
+
 import edu.wpi.first.wpilibj.command.Subsystem;
+import robot.RobotMap;
 
 public class ElevatorSubsystem extends Subsystem {
-    public TalonSRX liftMaster = new TalonSRX(14);
+
+    private TalonSRX liftMaster = new TalonSRX(RobotMap.ELEVATOR_MOTOR);
+
     public ElevatorSubsystem(){
 
+        liftMaster.configMotionSCurveStrength(4);
+        liftMaster.configSelectedFeedbackSensor(FeedbackDevice.CTRE_MagEncoder_Relative, 0,0);
+        liftMaster.setSensorPhase(ElevatorConstants.ENCODER_REVERSED);
+        liftMaster.config_kP(ElevatorConstants.TALON_PID_SLOT, 0.2, ElevatorConstants.TALON_TIMEOUT_MS);
+        liftMaster.config_kI(ElevatorConstants.TALON_PID_SLOT, 0, ElevatorConstants.TALON_TIMEOUT_MS);
+        liftMaster.config_kD(ElevatorConstants.TALON_PID_SLOT, 0, ElevatorConstants.TALON_TIMEOUT_MS);
+        liftMaster.config_kF(ElevatorConstants.TALON_PID_SLOT, 0, ElevatorConstants.TALON_TIMEOUT_MS);
     }
+    /*
+    public double getSpeed(){
+        return liftMaster.getSelectedSensorVelocity();
+    }
+
+     */
+    public void setPosition(double position){
+        liftMaster.set(ControlMode.MotionMagic, position, DemandType.ArbitraryFeedForward, ElevatorConstants.FEED_FORWARD);
+        
+    }
+    /*
+    public void setSpeed(double position){
+        liftMaster.set(ControlMode.MotionMagic, position);
+    }
+
+     */
 
     @Override
     protected void initDefaultCommand() {
