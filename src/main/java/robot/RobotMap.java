a04efef8146c3d36784a8a77da432be8e2ada517
Adam <adam.vichik@gmail.com>
2019-09-13 03:35:20 -0700

    Create minimum height

--- src/main/java/robot/RobotMap.java
+++ src/main/java/robot/RobotMap.java
@@ -3,5 +3,6 @@ package robot;
 public class RobotMap {
     public static final int ELEVATOR_MOTOR = 14;
     public static final double MAX_HEIGHT = 1.0;
+    public static final double MIN_HEIGHT = 0.0;
 
 }
