9dfa578c0b77a94bff49ca6c192bb7f24b4c047e
Barel Shlidor <barel@shlidor.org>
2019-10-04 16:17:21 +0300

    Fix inset cube to work until the command stops

--- src/main/java/robot/subsystems/gripper/commands/InsertCubeCommand.java
+++ src/main/java/robot/subsystems/gripper/commands/InsertCubeCommand.java
@@ -20,6 +20,7 @@ public class InsertCubeCommand extends Command {
     }
     // Called repeatedly when this Command is scheduled to run
     protected void execute() {
+        Robot.gripperSubsystem.setVelocities(-1, -1);
     }
 
     // Make this return true when this Command no longer needs to run execute()
