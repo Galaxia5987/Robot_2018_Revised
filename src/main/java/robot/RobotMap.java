80fee5878c3b4b5d30ca903736da7abb6c520db1
Paulo Khayat <paulokhayat@gmail.com>
2019-10-14 09:28:11 +0300

    Remove non-used constant in RobotMap
    This constant is already defined in elevator ports, and was probably left here by accident

--- src/main/java/robot/RobotMap.java
+++ src/main/java/robot/RobotMap.java
@@ -5,7 +5,6 @@ public class RobotMap {
     public static final int LEFT_MOTOR_PORT = 1;
     public static final int PROXIMITY_PORT = 5;
     public static final int ELEVATOR_MOTOR = 14;
-    public static final double MAX_HEIGHT = 1.0;
 
     public static final int LEFT_MASTER_PORT = 16;
     public static final int RIGHT_MASTER_PORT = 11;
