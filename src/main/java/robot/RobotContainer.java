c6a8d09afd18a9019d19098bd62f82b3c3fe56bf
Barel Shlidor <barel@shlidor.org>
2019-10-17 16:27:02 +0300

    Rename OI to RobotContainer

---
+++ src/main/java/robot/RobotContainer.java
@@ -0,0 +1,52 @@
+
+/* Copyright (c) 2017-2018 FIRST. All Rights Reserved.                        */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package robot;
+
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.wpilibj.buttons.Button;
+import edu.wpi.first.wpilibj.buttons.JoystickButton;
+import edu.wpi.first.wpilibj.command.Command;
+import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
+import robot.subsystems.intake.commands.IntakeIn;
+import robot.subsystems.intake.commands.MoveArms;
+import robot.subsystems.drivetrain.commands.DriveStraight;
+
+import static robot.subsystems.intake.commands.MoveArms.Direction;
+
+/**
+ * This class is the glue that binds the controls on the physical operator
+ * interface to the commands and command groups that allow control of the robot.
+ */
+public class RobotContainer {
+    public XboxController xbox = new XboxController(2);
+    Button a = new JoystickButton(xbox, 1);
+    Button b = new JoystickButton(xbox, 2);
+    Button x = new JoystickButton(xbox, 3);
+    Button y = new JoystickButton(xbox, 4);
+    private Joystick right = new Joystick(0);
+    Button triggerRight = new JoystickButton(right, 7);
+    private Joystick left = new Joystick(1);
+    Button triggerLeft = new JoystickButton(left, 6);
+    private Button left10 = new JoystickButton(left, 10);
+    private Button right11 = new JoystickButton(right, 11);
+    SendableChooser<Command> m_chooser = new SendableChooser<>();
+
+
+    public RobotContainer() {
+        b.whenPressed(new DriveStraight(0.4));
+        x.whenPressed(new DriveStraight(-0.4));
+        left10.whenPressed(new MoveArms(Direction.UP));
+        right11.whenPressed(new MoveArms(Direction.DOWN));
+        a.whenPressed(new IntakeIn(-1, 6));
+    }
+
+    public Command getAutonomous() {
+        return m_chooser.getSelected();
+    }
+}
