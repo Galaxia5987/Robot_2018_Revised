37e8a80ce2b5c7950ddbd348081343f2a99b7bd2
Barel Shlidor <barel@shlidor.org>
2019-10-17 16:31:03 +0300

    Create Constants class

---
+++ src/main/java/robot/Constants.java
@@ -0,0 +1,19 @@
+package robot;
+
+public class Constants {
+    public static class Drivetrain {
+
+    }
+
+    public static class Elevator {
+
+    }
+
+    public static class Gripper {
+
+    }
+
+    public static class Intake {
+
+    }
+}
