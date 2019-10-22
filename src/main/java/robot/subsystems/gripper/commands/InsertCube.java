ef0f827db75b76912ba4471e22199486efbbecaa
Paulo Khayat <paulokhayat@gmail.com>
2019-10-22 18:09:45 +0300

    Change setVelocity to setSpeed

--- src/main/java/robot/subsystems/gripper/commands/InsertCube.java
+++ src/main/java/robot/subsystems/gripper/commands/InsertCube.java
@@ -16,12 +16,12 @@ public class InsertCube extends Command {
     protected void initialize() {
         timer.reset();
         timer.start();
-        Robot.gripper.setVelocities(-1, -1);
+        Robot.gripper.setSpeed(-1);
     }
 
     // Called repeatedly when this Command is scheduled to run
     protected void execute() {
-        Robot.gripper.setVelocities(-1, -1);
+        Robot.gripper.setSpeed(-1);
     }
 
     // Make this return true when this Command no longer needs to run execute()
@@ -31,7 +31,7 @@ public class InsertCube extends Command {
 
     // Called once after isFinished returns true
     protected void end() {
-        Robot.gripper.setVelocities(0, 0);
+        Robot.gripper.setSpeed(0);
     }
 
     // Called when another command which requires one or more of the same
