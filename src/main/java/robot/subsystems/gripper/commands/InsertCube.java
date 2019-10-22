3de19940373c89f5901216674c6bc391542bd363
Barel Shlidor <barel@shlidor.org>
2019-10-22 18:47:33 +0300

    Use constant speed instead magic number

--- src/main/java/robot/subsystems/gripper/commands/InsertCube.java
+++ src/main/java/robot/subsystems/gripper/commands/InsertCube.java
@@ -4,6 +4,8 @@ import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.command.Command;
 import robot.Robot;
 
+import static robot.Constants.Gripper.DEFAULT_SPEED;
+
 public class InsertCube extends Command {
     private Timer timer = new Timer();
 
@@ -12,30 +14,29 @@ public class InsertCube extends Command {
     }
 
     // Called just before this Command runs the first time
+    @Override
     protected void initialize() {
         timer.reset();
         timer.start();
-        Robot.gripper.setVelocities(-1, -1);
+        Robot.gripper.setVelocities(-DEFAULT_SPEED, -DEFAULT_SPEED);
     }
 
     // Called repeatedly when this Command is scheduled to run
+    @Override
     protected void execute() {
-        Robot.gripper.setVelocities(-1, -1);
+        Robot.gripper.setVelocities(-DEFAULT_SPEED, -DEFAULT_SPEED);
     }
 
     // Make this return true when this Command no longer needs to run execute()
+    @Override
     protected boolean isFinished() {
         return timer.get() >= Robot.gripper.getTimeout() || Robot.gripper.isCubeInside();
     }
 
     // Called once after isFinished returns true
+    @Override
     protected void end() {
         Robot.gripper.setVelocities(0, 0);
     }
 
-    // Called when another command which requires one or more of the same
-    // subsystems is scheduled to run
-    protected void interrupted() {
-        end();
-    }
 }
