015cff1853fe4ca84c661198aaab8a5337bf473e
Barel Shlidor <barel@shlidor.org>
2019-10-22 18:22:22 +0300

    Use gripper timeout in Insert Cube command

--- src/main/java/robot/subsystems/gripper/commands/InsertCube.java
+++ src/main/java/robot/subsystems/gripper/commands/InsertCube.java
@@ -5,7 +5,6 @@ import edu.wpi.first.wpilibj.command.Command;
 import robot.Robot;
 
 public class InsertCube extends Command {
-    private final double timeout = 5;
     private Timer timer = new Timer();
 
     public InsertCube() {
@@ -26,7 +25,7 @@ public class InsertCube extends Command {
 
     // Make this return true when this Command no longer needs to run execute()
     protected boolean isFinished() {
-        return timer.get() >= timeout || Robot.gripper.isCubeInside();
+        return timer.get() >= Robot.gripper.getTimeout() || Robot.gripper.isCubeInside();
     }
 
     // Called once after isFinished returns true
