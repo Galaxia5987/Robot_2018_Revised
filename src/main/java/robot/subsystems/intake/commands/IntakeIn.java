ea39c836b6a38c066b572f9df06a231c818589d0
Adam <adam.vichik@gmail.com>
2019-10-27 17:08:32 +0200

    Change getCubeDistance() to isCubedInside()

--- src/main/java/robot/subsystems/intake/commands/IntakeIn.java
+++ src/main/java/robot/subsystems/intake/commands/IntakeIn.java
@@ -23,7 +23,7 @@ public class IntakeIn extends Command {
 
     @Override
     protected void execute() {
-        if (Robot.elevator.getHeight() <= INTAKE_THRESHOLD && !Robot.elevator.areArmsFolded() && Robot.gripper.getCubeDistance() <= MIN_CUBE_DISTANCE) {
+        if (Robot.elevator.getHeight() <= INTAKE_THRESHOLD && !Robot.elevator.areArmsFolded() && Robot.gripper.isCubeInside()) {
             Robot.intake.setSpeed(INTAKING_SPEED);
             Robot.gripper.setSpeed(DEFAULT_SPEED);
         }
@@ -31,7 +31,7 @@ public class IntakeIn extends Command {
 
     @Override
     protected boolean isFinished() {
-        return timer.get() > INTAKE_OPERATION_TIME || Robot.elevator.getHeight() > INTAKE_THRESHOLD || !Robot.elevator.areArmsFolded() || Robot.gripper.getCubeDistance() > MIN_CUBE_DISTANCE;
+        return timer.get() > INTAKE_OPERATION_TIME || Robot.elevator.getHeight() > INTAKE_THRESHOLD || !Robot.elevator.areArmsFolded() || !Robot.gripper.isCubeInside();
     }
 
     @Override
