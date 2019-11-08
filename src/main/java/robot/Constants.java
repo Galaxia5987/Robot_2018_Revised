f749d8e40da4d6693417f779c85987298603914d
Paulo Khayat <paulokhayat@gmail.com>
2019-11-08 12:58:37 +0200

    Change Intake inverted constants

--- src/main/java/robot/Constants.java
+++ src/main/java/robot/Constants.java
@@ -41,6 +41,6 @@ public class Constants {
         public static final int SOLENOID_FORWARD = 0;
         public static final int SOLENOID_REVERSE = 1;
         public static final boolean LEFT_REVERSED = false;
-        public static final boolean RIGHT_REVERSED = false;
+        public static final boolean RIGHT_REVERSED = true;
     }
 }
