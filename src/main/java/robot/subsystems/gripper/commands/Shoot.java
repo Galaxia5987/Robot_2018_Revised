b1e5bb2e0e64c9faf7b9da84e5e2a825f1fa2ccc
Paulo Khayat <paulokhayat@gmail.com>
2019-11-08 13:29:07 +0200

    Change checking for intake arms in Shoot command

--- src/main/java/robot/subsystems/gripper/commands/Shoot.java
+++ src/main/java/robot/subsystems/gripper/commands/Shoot.java
@@ -5,7 +5,6 @@ import edu.wpi.first.wpilibj.command.InstantCommand;
 import robot.Robot;
 
 import static robot.Constants.Gripper.MIN_SHOOTING_HEIGHT;
-import static robot.Robot.intake;
 
 public class Shoot extends InstantCommand {
     private Timer timer = new Timer();
@@ -13,7 +12,7 @@ public class Shoot extends InstantCommand {
 
     public Shoot(double speed, double timeout) {
         requires(Robot.gripper);
-        requires(intake);
+        requires(Robot.intake);
         this.speed = speed;
         this.timeout = timeout;
     }
@@ -38,15 +37,15 @@ public class Shoot extends InstantCommand {
 
     private void applyPower() {
         if (Robot.elevator.getHeight() > MIN_SHOOTING_HEIGHT)
-            if (intake.areArmsFolded()) {
+            if (Robot.intake.areArmsFolded()) {
                 Robot.gripper.setSpeed(speed);
-                intake.setSpeed(speed);
+                Robot.intake.setSpeed(speed);
             }
     }
 
     @Override
     protected boolean isFinished() {
-        return timeout >= timer.get() || intake.areArmsFolded();
+        return timeout >= timer.get() || Robot.intake.areArmsFolded();
     }
 
     // Called once after isFinished returns true
@@ -54,6 +53,6 @@ public class Shoot extends InstantCommand {
     protected void end() {
         timer.stop();
         Robot.gripper.setSpeed(0);
-        intake.setSpeed(0);
+        Robot.intake.setSpeed(0);
     }
 }
